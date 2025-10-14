88282b2808fce9a71771ece5eab14d76455e477b
Julian Carlin <julianmcarlin@gmail.com>
2025-10-14 15:20:00 -0600

    tweak scroll snap settings

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-    <main className="snap-y snap-mandatory h-screen overflow-y-auto">
+    <main className="snap-y snap-proximity h-screen overflow-y-auto">
       <Hero />
       <Services />
       <Projects />
